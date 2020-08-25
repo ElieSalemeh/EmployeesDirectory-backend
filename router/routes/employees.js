4699b6a45d8387b54e2c2b87e4bc084f40a6bd1c
Elie Salemeh <eliesalameh_1@hotmail.com>
2020-08-25 10:17:51 +0300

    Add search route

--- router/routes/employees.js
+++ router/routes/employees.js
@@ -10,7 +10,7 @@ router.get('/allEmployees/:page/:pageSize', async (req, res) => {
 		const pageSize = +req.params.pageSize;
 
 		const results = await employees.find().skip(page * pageSize).limit(pageSize).lean().exec();
-		res.send({employees: results});
+		res.send({ employees: results });
 	} catch (error) {
 		res.send(error);
 	}
@@ -21,8 +21,8 @@ router.get('/allEmployees/:page/:pageSize', async (req, res) => {
 router.get('/countEmployees', async (req, res) => {
 	try {
 		const count = await employees.countDocuments().exec();
-		res.send({count});
-	}catch(error) {
+		res.send({ count });
+	} catch (error) {
 		res.send(error);
 	}
 });
@@ -30,12 +30,33 @@ router.get('/countEmployees', async (req, res) => {
 
 // Get Employee by Id 
 router.get('/employeeById/:employeeId', async (req, res) => {
-	try {	
+	try {
 		const employeeId = req.params.employeeId;
 
 		const employee = await employees.findById(employeeId).lean().exec();
-		res.send({employee});
-	}catch (error) {
+		res.send({ employee });
+	} catch (error) {
+		res.send(error);
+	}
+});
+
+// Search Employees
+router.get('/searchEmployees/:searchText', async (req, res) => {
+	try {
+		const searchText = req.params.searchText;
+		const txtRegex = new RegExp(`^${searchText}`, 'i')
+		const results = await employees
+			.find({
+				$or: [
+					{ firstName: txtRegex },
+					{ lastName: txtRegex },
+					{ jobTitle: txtRegex },
+					{ department: txtRegex },
+					{ address: txtRegex }
+				]
+			}).lean().exec();
+		res.send({ results });
+	} catch (error) {
 		res.send(error);
 	}
 });
@@ -44,10 +65,10 @@ router.get('/employeeById/:employeeId', async (req, res) => {
 // Create Employee 
 router.post('/employee', async (req, res) => {
 	try {
-		const employee = req.body; 
+		const employee = req.body;
 		const results = await employees.create(employee);
 		res.send(results);
-	}catch(error) {
+	} catch (error) {
 		res.send(error);
 	}
 });
@@ -60,18 +81,18 @@ router.put('/employee', async (req, res) => {
 		const employeeObj = req.body.employeeObj;
 		const results = await employees.findByIdAndUpdate(employeeId, employeeObj).lean().exec();
 		res.send(results);
-	}catch(error) {
+	} catch (error) {
 		res.send(error);
 	}
 });
 
 // Delete Employee 
 router.delete('/employee/:employeeId', async (req, res) => {
-	try{
+	try {
 		const employeeId = req.params.employeeId;
 		const results = await employees.findByIdAndRemove(employeeId).lean().exec();
-		res.send(results);
-	}catch(error) {
+		res.sendSatus(200);
+	} catch (error) {
 		res.send(error);
 	}
 });
